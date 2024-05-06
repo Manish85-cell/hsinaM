677bfbf667ba4246a19f5d3d61838d4eebd84e5e
Manish <mupreti2002@gmail.com>
2024-05-06 21:21:34 +0530

    2nd Day Work

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index , name="about")
+    path('', views.index , name="index"),
+    path('intro', views.intro, name="intro"),
+    path("blog", views.blog, name="blog"),
+    path("cv", views.cv, name="cv"),
+    path("contact", views.contact, name="contact")
 ]
