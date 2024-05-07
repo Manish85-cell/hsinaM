983e1f9ac44c371f4c1374375ea856935610b481
Manish <mupreti2002@gmail.com>
2024-05-07 22:00:51 +0530

    deploy on vercel

--- hsinaM/settings.py
+++ hsinaM/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
